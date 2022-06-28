f21d7ad6dfa66ec06eae1cee50a65b035635d86f
nekoimi <nekoimime@gmail.com>
2022-06-28 17:45:25 +0800

    :ambulance: use root start supervisord

--- docker-entrypoint.d/00-supervisor-entrypoint.sh
+++ docker-entrypoint.d/00-supervisor-entrypoint.sh
@@ -32,9 +32,28 @@ fi
 _main() {
   exec_config_supervisor
 
+#logfile=/var/log/supervisord.log ; (main log file;default $CWD/supervisord.log)
+#  ;logfile_maxbytes=50MB       ; (max main logfile bytes b4 rotation;default 50MB)
+#  ;logfile_backups=10          ; (num of main logfile rotation backups;default 10)
+#  loglevel=info                ; (log level;default info; others: debug,warn,trace)
+#pidfile=/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
+#  ;nodaemon=false              ; (start in foreground if true;default false)
+#  ;minfds=1024                 ; (min. avail startup file descriptors;default 1024)
+#  ;minprocs=200                ; (min. avail process descriptors;default 200)
+#  ;umask=022                   ; (process file creation umask;default 022)
+#user=chrism                 ; (default is current user, required if root)
+#  ;identifier=supervisor       ; (supervisord identifier, default is 'supervisor')
+#  ;directory=/tmp              ; (default is not to cd during start)
+#  ;nocleanup=true              ; (don't clean up tempfiles at start;default false)
+#  ;childlogdir=/var/log/supervisor ; ('AUTO' child log dir, default $TEMP)
+#  ;environment=KEY=value       ; (key value pairs to add to environment)
+#  ;strip_ansi=false            ; (strip ansi escape codes in logs; def. false)
+
+
   if [ -f /etc/supervisord.conf ]; then
-    sed -i 's/;pidfile=\/run\/supervisord.pid/pidfile=\/run\/supervisord.pid/g' /etc/supervisord.conf
-    sed -i 's/;childlogdir=\/var\/log\/supervisor/childlogdir=\/var\/log\/supervisor/g' /etc/supervisord.conf
+    sed -i 's#;user=chrism#user=root#g' /etc/supervisord.conf
+    sed -i 's#;pidfile=/run/supervisord.pid#pidfile=/run/supervisord.pid#g' /etc/supervisord.conf
+    sed -i 's#;childlogdir=/var/log/supervisor#childlogdir=/var/log/supervisor#g' /etc/supervisord.conf
   fi
 }
 
